346cdd2e626f445431f928aec2736adaa94a3c33
Wmcurr <66012968+Wmcurr@users.noreply.github.com>
2024-11-20 01:28:30 +0300

    Update url.php

--- kohana-3.4.2/kohana-3.4.2/system/config/url.php
+++ kohana-3.4.2/kohana-3.4.2/system/config/url.php
@@ -1,10 +1,8 @@
-<?php
-
-return [
-    'trusted_hosts' => [
-       // все поддомены вашего дополнительного домена
-        'test\.online',       // еще один дополнительный домен
-        '.*\.test\.online',   // все поддомены еще одного дополнительного домена
-    ],
-];
-
+return array (
+  'trusted_hosts' => 
+  array (
+    'test.com',
+    '*.test.com',
+  ),
+  'hmac_key' => 'null',
+);
