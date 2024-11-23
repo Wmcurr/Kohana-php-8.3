6953346b44c3f2c6d90525403648cc1ec3af6169
Wmcurr <66012968+Wmcurr@users.noreply.github.com>
2024-11-23 06:12:35 +0300

    Update Join.php

--- kohana-3.4.2/kohana-3.4.2/modules/database/classes/Kohana/Database/Query/Builder/Join.php
+++ kohana-3.4.2/kohana-3.4.2/modules/database/classes/Kohana/Database/Query/Builder/Join.php
@@ -1,53 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Database query builder for JOIN statements. See [Query Builder](/database/query/builder) for usage and examples.
  *
  * @package    Kohana/Database
  * @category   Query
- * @author     Kohana Team
- * @copyright  (c) 2008-2009 Kohana Team
- * @license    https://kohana.top/license
  */
-class Kohana_Database_Query_Builder_Join extends Database_Query_Builder
+class Kohana_Database_Query_Builder_Join extends Kohana_Database_Query
 {
-    // Type of JOIN
-    protected $_type;
     // JOIN ...
-    protected $_table;
+    protected string|array $_table;
     // ON ...
-    protected $_on = [];
+    protected array $_on = [];
     // USING ...
-    protected $_using = [];
+    protected array $_using = [];
 
     /**
      * Creates a new JOIN statement for a table. Optionally, the type of JOIN
      * can be specified as the second parameter.
      *
-     * @param   mixed   $table  column name or [$column, $alias] or object
-     * @param   string  $type   type of JOIN: INNER, RIGHT, LEFT, etc
-     * @return  void
+     * @param string|array $table Column name or [$column, $alias] or object.
+     * @param QueryType|null $type Type of JOIN: INNER, RIGHT, LEFT, etc.
      */
-    public function __construct($table, $type = null)
+    public function __construct(string|array $table, ?QueryType $type = null)
     {
+        // Call parent constructor with the given type or default to INNER JOIN
+        parent::__construct($type ?? QueryType::INNER, '');
+
         // Set the table to JOIN on
         $this->_table = $table;
-
-        if ($type !== null) {
-            // Set the JOIN type
-            $this->_type = (string) $type;
-        }
     }
 
     /**
      * Adds a new condition for joining.
      *
-     * @param   mixed   $c1  column name or [$column, $alias] or object
-     * @param   string  $op  logic operator
-     * @param   mixed   $c2  column name or [$column, $alias] or object
-     * @return  $this
+     * @param string|array|Database_Expression $c1  Column name or [$column, $alias] or object.
+     * @param string                           $op  Logic operator.
+     * @param string|array|Database_Expression $c2  Column name or [$column, $alias] or object.
+     * @return $this
+     * @throws Kohana_Exception
      */
-    public function on($c1, $op, $c2)
+    public function on(string|array|Database_Expression $c1, string $op, string|array|Database_Expression $c2): self
     {
         if (!empty($this->_using)) {
             throw new Kohana_Exception('JOIN ... ON ... cannot be combined with JOIN ... USING ...');
@@ -59,18 +54,17 @@ class Kohana_Database_Query_Builder_Join extends Database_Query_Builder
     }
 
     /**
-     * Adds a new condition for joining.
+     * Adds a new condition for joining using columns.
      *
-     * @param   string  $columns  column name
-     * @return  $this
+     * @param string ...$columns Column names.
+     * @return $this
+     * @throws Kohana_Exception
      */
-    public function using($columns)
+    public function using(string ...$columns): self
     {
         if (!empty($this->_on)) {
             throw new Kohana_Exception('JOIN ... ON ... cannot be combined with JOIN ... USING ...');
         }
-
-        $columns = func_get_args();
 
         $this->_using = array_merge($this->_using, $columns);
 
@@ -80,55 +74,53 @@ class Kohana_Database_Query_Builder_Join extends Database_Query_Builder
     /**
      * Compile the SQL partial for a JOIN statement and return it.
      *
-     * @param   mixed  $db  Database instance or name of instance
-     * @return  string
+     * @param mixed $db Database instance or name of instance.
+     * @return string
      */
-    public function compile($db = null)
+    public function compile(mixed $db = null): string
     {
         if (!is_object($db)) {
             // Get the database instance
             $db = Database::instance($db);
         }
 
-        if ($this->_type) {
-            $sql = strtoupper($this->_type) . ' JOIN';
-        } else {
-            $sql = 'JOIN';
-        }
+        // Ensure that $_type is correctly initialized and accessible
+        $sql = strtoupper($this->_type->name) . ' JOIN';
 
         // Quote the table name that is being joined
         $sql .= ' ' . $db->quote_table($this->_table);
 
         if (!empty($this->_using)) {
-            // Quote and concat the columns
+            // Quote and concatenate the columns
             $sql .= ' USING (' . implode(', ', array_map([$db, 'quote_column'], $this->_using)) . ')';
         } else {
             $conditions = [];
-            foreach ($this->_on as $condition) {
-                // Split the condition
-                list($c1, $op, $c2) = $condition;
-
-                if ($op) {
-                    // Make the operator uppercase and spaced
-                    $op = ' ' . strtoupper($op);
-                }
+            foreach ($this->_on as [$c1, $op, $c2]) {
+                // Make the operator uppercase and spaced
+                $op = $op ? ' ' . strtoupper($op) : '';
 
                 // Quote each of the columns used for the condition
                 $conditions[] = $db->quote_column($c1) . $op . ' ' . $db->quote_column($c2);
             }
 
-            // Concat the conditions "... AND ..."
+            // Concatenate the conditions "... AND ..."
             $sql .= ' ON (' . implode(' AND ', $conditions) . ')';
         }
 
         return $sql;
     }
 
-    public function reset()
+    /**
+     * Reset the join builder to its initial state.
+     *
+     * @return $this
+     */
+    public function reset(): self
     {
-        $this->_type = $this->_table = null;
-
+        $this->_table = [];
         $this->_on = [];
-    }
+        $this->_using = [];
 
+        return $this;
+    }
 }
