651b79101fd03b8072e144c28140f2892c839bec
Wmcurr <66012968+Wmcurr@users.noreply.github.com>
2024-11-23 06:12:08 +0300

    Update Insert.php

--- kohana-3.4.2/kohana-3.4.2/modules/database/classes/Kohana/Database/Query/Builder/Insert.php
+++ kohana-3.4.2/kohana-3.4.2/modules/database/classes/Kohana/Database/Query/Builder/Insert.php
@@ -1,44 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Database query builder for INSERT statements. See [Query Builder](/database/query/builder) for usage and examples.
  *
  * @package    Kohana/Database
  * @category   Query
  * @author     Kohana Team
- * @copyright  (c) 2008-2009 Kohana Team
- * @license    https://kohana.top/license
+ * @copyright  (c) 2024
+ * @php 8.3
  */
 class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
 {
     // INSERT INTO ...
-    protected $_table;
+    protected ?string $_table = null;
     // (...)
-    protected $_columns = [];
+    protected array $_columns = [];
     // VALUES (...)
-    protected $_values = [];
+    protected array $_values = [];
 
     /**
      * Set the table and columns for an insert.
      *
      * @param   mixed  $table    table name or [$table, $alias] or object
      * @param   array  $columns  column names
-     * @return  void
      */
-    public function __construct($table = null, array $columns = null)
+    public function __construct(mixed $table = null, ?array $columns = null)
     {
-        if ($table) {
-            // Set the inital table name
+        if ($table !== null) {
+            // Set the initial table name
             $this->table($table);
         }
 
-        if ($columns) {
+        if ($columns !== null) {
             // Set the column names
             $this->_columns = $columns;
         }
 
         // Start the query with no SQL
-        return parent::__construct(Database::INSERT, '');
+        parent::__construct(Database::INSERT, '');
     }
 
     /**
@@ -47,10 +48,11 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
      * @param   string  $table  table name
      * @return  $this
      */
-    public function table($table)
+    public function table(string $table): static
     {
-        if (!is_string($table))
+        if (!is_string($table)) {
             throw new Kohana_Exception('INSERT INTO syntax does not allow table aliasing');
+        }
 
         $this->_table = $table;
 
@@ -63,7 +65,7 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
      * @param   array  $columns  column names
      * @return  $this
      */
-    public function columns(array $columns)
+    public function columns(array $columns): static
     {
         $this->_columns = $columns;
 
@@ -73,11 +75,10 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
     /**
      * Adds or overwrites values. Multiple value sets can be added.
      *
-     * @param   array   $values  values list
-     * @param   ...
+     * @param   array  $values  values list
      * @return  $this
      */
-    public function values(array $values)
+    public function values(array $values): static
     {
         if (!is_array($this->_values)) {
             throw new Kohana_Exception('INSERT INTO ... SELECT statements cannot be combined with INSERT INTO ... VALUES');
@@ -94,12 +95,12 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
     }
 
     /**
-     * Use a sub-query to for the inserted values.
+     * Use a sub-query for the inserted values.
      *
-     * @param   object  $query  Database_Query of SELECT type
+     * @param   Database_Query  $query  Database_Query of SELECT type
      * @return  $this
      */
-    public function select(Database_Query $query)
+    public function select(Database_Query $query): static
     {
         if ($query->type() !== Database::SELECT) {
             throw new Kohana_Exception('Only SELECT queries can be combined with INSERT queries');
@@ -116,11 +117,15 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
      * @param   mixed  $db  Database instance or name of instance
      * @return  string
      */
-    public function compile($db = null)
+    public function compile(mixed $db = null): string
     {
         if (!is_object($db)) {
             // Get the database instance
             $db = Database::instance($db);
+        }
+
+        if ($this->_table === null) {
+            throw new Kohana_Exception('No table specified for insert query');
         }
 
         // Start an insertion query
@@ -136,7 +141,7 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
             $groups = [];
             foreach ($this->_values as $group) {
                 foreach ($group as $offset => $value) {
-                    if ((is_string($value) AND array_key_exists($value, $this->_parameters)) === false) {
+                    if (is_string($value) && !array_key_exists($value, $this->_parameters)) {
                         // Quote the value, it is not a parameter
                         $group[$offset] = $db->quote($value);
                     }
@@ -152,23 +157,22 @@ class Kohana_Database_Query_Builder_Insert extends Database_Query_Builder
             $query .= (string) $this->_values;
         }
 
-        $this->_sql = $query;
-
-        return parent::compile($db);
-        ;
+        return $query;
     }
 
-    public function reset()
+    /**
+     * Reset the current builder state.
+     *
+     * @return  $this
+     */
+    public function reset(): static
     {
         $this->_table = null;
-
-        $this->_columns = $this->_values = [];
-
+        $this->_columns = [];
+        $this->_values = [];
         $this->_parameters = [];
-
         $this->_sql = null;
 
-        return $this;
+        return parent::reset();
     }
-
 }
